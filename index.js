107114d865cfd061381c6880bc985fba6e9df34e
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-04-08 20:25:19 +0530

    second

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 let container =document.getElementById("container")
 
-window.addEventListener("scroll",()=>{
+container.addEventListener("scroll",()=>{
     setTimeout(()=>{
 
-        loadContent(25)
-    },500)
+        if(container.scrollHeight -container.scrollTop -container.clientHeight <1){
+            loadContent()
+
+        }
+    },800)
 })
-loadContent(25)
 
+let count=1
+loadContent()
+// let scrollY = window.scrollY
+// let innerHeight =window.innerHeight
+// let offsetHeight =document.body.offsetHeight
 
-function loadContent(num){
-    let scrollY = window.scrollY
-    let innerHeight =window.innerHeight
+// if(scrollY + innerHeight> offsetHeight-100){
+   
+// }
+function loadContent(){
+
+    
+        let item =1
+        let div = document.createElement("div")
+    
+       while(item<=25){
+            let h3 =document.createElement("h3")
+            h3.innerText ="Masai School   " +count
+            div.append(h3)
+            container.append(div)
+            count++
+            item++
+        }
+    
 }
